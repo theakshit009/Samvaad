d8436bbe952ab87dcad6e396e2a31bb44d11f1c2
Akshit Panchal <akshitpp01@gmail.com>
2025-08-03 23:08:00 +0530

    Deplyments files

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
-import { AppProvider } from '../context/AppContext.jsx'
-import { ChatProvider } from '../context/ChatContext.jsx'
+import { AppProvider } from '../context/AppProvider.jsx'
+import { ChatProvider } from '../context/ChatProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
